ad5392ff1accdf1919ec7797913fdd5105fbe3bd
Peter Rhodes <petrov82@aim.com>
2014-03-03 13:32:55 -0600

    add visibility standards

--- filestore.php
+++ filestore.php
@@ -4,15 +4,18 @@ class Filestore {
 
     public $filename = '';
 
-    function __construct($filename = '') 
+    private $is_csv = FALSE;
+
+    public function __construct($filename = '') 
     {
         // Sets $this->filename
+        $this->filename = $filename;
     }
 
     /**
      * Returns array of lines in $this->filename
      */
-    function read_lines()
+    private function read_lines()
     {
 
     }
@@ -20,7 +23,7 @@ class Filestore {
     /**
      * Writes each element in $array to a new line in $this->filename
      */
-    function write_lines($array)
+    private function write_lines($array)
     {
 
     }
@@ -28,17 +31,53 @@ class Filestore {
     /**
      * Reads contents of csv $this->filename, returns an array
      */
-    function read_csv()
+    private function read_csv()
     {
-
+        $address_book = [];
+		if (($handle = fopen($this->filename, "r")) !== FALSE) {
+			while (($fields = fgetcsv($handle)) !== FALSE) {
+				array_push($address_book, $fields);
+				//echo gettype($fields);
+			}
+	    	fclose($handle);
+		}
+		return $address_book;
     }
 
     /**
      * Writes contents of $array to csv $this->filename
      */
-    function write_csv($array)
+    private function write_csv($entry)
     {
+		$handle = fopen($this->filename, 'w');
+		foreach ($entry as $item) {
+	    	fputcsv($handle, $item);
+			}
+	    fclose($handle);
+    }
 
+    public function open() 
+    {
+        if ($is_csv = TRUE) {
+            return $this->read_csv();
+
+        }
+        else {
+            return $this->read_lines();
+
+        }
+    }
+
+    public function write($array) 
+    {
+        if ($is_csv = TRUE) {
+            return $this->write_csv($array);
+
+        }
+        else {
+            return $this->write_lines($array);
+
+        }
     }
 
 }
